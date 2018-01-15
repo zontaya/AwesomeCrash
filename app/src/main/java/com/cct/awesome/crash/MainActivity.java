4a087394c7f87e044d62a5986b557402aed3db7b
zontaya <zontaya@gmail.com>
2018-01-15 14:20:55 +0700

    Initial commit

---
+++ app/src/main/java/com/cct/awesome/crash/MainActivity.java
@@ -0,0 +1,39 @@
+package com.cct.awesome.crash;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+                logUser();
+                Crashlytics.getInstance().crash(); // ทดลองทำให้พังซะหน่อย
+            }
+        });
+    }
+    private void logUser() {
+        Crashlytics.setUserIdentifier("999");
+        Crashlytics.setUserEmail("user@fabric.io");
+        Crashlytics.setUserName("BAS");
+    }
+
+}
